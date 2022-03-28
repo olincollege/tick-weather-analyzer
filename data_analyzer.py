608cab43befc9fbf80213c27928f8c7282851756
Lili Baker <lilijbaker@gmail.com>
2022-03-27 21:55:58 -0400

    Edit tick_analyze to output pandas series.
    Adit tick_analyze so that every function in data_analyzer returns
    the same datatype for plotting purposes.

--- data_analyzer.py
+++ data_analyzer.py
@@ -9,9 +9,9 @@ def humidity_analyze(data):
         data: Relative humidity dataframe, downloaded from neon_data_tools
 
     Returns:
-        necessary_data: a dataframe with the second column as the average
-        humidity over every day of a month & the first column as the month
-        and year the corresponding data on the second column is collected
+        necessary_data: a pandas series of the average
+        humidity over every day of a month with the month as the
+        row labels.
     
     """
     #Convert contents of "startDateTime" to a datetime object
@@ -21,8 +21,9 @@ def humidity_analyze(data):
     #Average the humidity for each month
     avg_humidity = data['RHMean'].resample('M').mean()
 
-    #Return array with the month and average relative humidity for each month
+ 
     return avg_humidity
+
 
 def precipitation_analyze(data):
     """
@@ -33,9 +34,9 @@ def precipitation_analyze(data):
         data: Precipitation dataframe, downloaded from neon_data_tools
 
     Returns:
-        necessary_data: a 2D array with the second column as the average
-        precipitation over every day of a month & the first column as the month
-        and year the corresponding data on the second column is collected
+        necessary_data: a pandas series of the average
+        precipitation over every day of a month with the month as the
+        row labels.
     
     """
     #Convert contents of "startDateTime" to a datetime object
@@ -51,6 +52,7 @@ def precipitation_analyze(data):
     #Return array with the month and average precipitation for each month
     return precipitation
 
+
 def tick_analyze(data):
     """
     Isolate and process only necessary tick data
@@ -58,26 +60,14 @@ def tick_analyze(data):
     Args:
         data: Ticks dataframe, downloaded from neon_data_tools
     Returns:
-        necessary_data: a 2D array with the second row as the total amount of tick
-        for a month and the first row as the month and year the data is collected
+        necessary_data: a pandas series with the total amount of tick
+        for a month and the month as the row labels.
     """
-    # Collect dates and tick data from the dataframe containing ticks data (data[0])
-    dates = data.loc[:,"collectDate"].values.tolist()
-    amounts = data.loc[:,"individualCount"].values.tolist()
-    
-    # Initialize return list
-    necessary_data = [[],[]]
+    #Convert contents of "collectDate" to a datetime object
+    data["collectDate"] = pd.to_datetime(data["collectDate"])
+    data.set_index("collectDate", inplace = True)
 
-    # Loop through data
-    for i in range(len(dates)):
-        # Combine ticks data from the same month
-        if dates[i][:7] == dates[i-1][:7]:
-            total_amount += amounts[i]
-        elif dates[i][:7] != dates[i-1][:7]:
-            # Add total amount per month at the end of a month
-            if i != 0:
-                necessary_data[0].append(dates[i-1][:7])
-                necessary_data[1].append(total_amount)
-            # Reset counter
-            total_amount = 0
-    return necessary_data
+    #Sum the ticks for the month
+    sum_ticks = data['individualCount'].resample('M').sum()
+
+    return sum_ticks
