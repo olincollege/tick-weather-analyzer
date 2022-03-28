33c1cb2b71ce0a50d084d7823266a30d5f703e14
Cherry Pham <meokkmun@gmail.com>
2022-03-27 22:40:10 -0400

    Run pylint and edit style issues

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,10 +1,14 @@
+"""
+Process necessary data from dataframes pulled from the National Ecological Observatory Network
+"""
 import pandas as pd
+
 
 def humidity_analyze(data):
     """
     Isolate and process only necessary humidity data by averaging all
     values for each month
-    
+
     Args:
         data: Relative humidity dataframe, downloaded from neon_data_tools
 
@@ -12,16 +16,15 @@ def humidity_analyze(data):
         necessary_data: a pandas series of the average
         humidity over every day of a month with the month as the
         row labels.
-    
-    """
-    #Convert contents of "startDateTime" to a datetime object
-    data["startDateTime"] = pd.to_datetime(data["startDateTime"])
-    data.set_index("startDateTime", inplace = True)
 
-    #Average the humidity for each month
+    """
+    # Convert contents of "startDateTime" to a datetime object
+    data["startDateTime"] = pd.to_datetime(data["startDateTime"])
+    data.set_index("startDateTime", inplace=True)
+
+    # Average the humidity for each month
     avg_humidity = data['RHMean'].resample('M').mean()
 
- 
     return avg_humidity
 
 
@@ -29,7 +32,7 @@ def precipitation_analyze(data):
     """
     Isolate and process only necessary precipitation data by adding total
     precipitation for each day and then averaging the totals for each month
-    
+
     Args:
         data: Precipitation dataframe, downloaded from neon_data_tools
 
@@ -37,37 +40,37 @@ def precipitation_analyze(data):
         necessary_data: a pandas series of the average
         precipitation over every day of a month with the month as the
         row labels.
-    
+
     """
-    #Convert contents of "startDateTime" to a datetime object
+    # Convert contents of "startDateTime" to a datetime object
     data["startDateTime"] = pd.to_datetime(data["startDateTime"])
-    data.set_index("startDateTime", inplace = True)
-    
-    #Add the total precipitation for each day
+    data.set_index("startDateTime", inplace=True)
+
+    # Add the total precipitation for each day
     precipitation = data["priPrecipBulk"].resample('D').sum()
 
-    #Average the precipitation for the month using the total of each day
+    # Average the precipitation for the month using the total of each day
     precipitation = precipitation.resample('M').mean()
 
-    #Return array with the month and average precipitation for each month
+    # Return array with the month and average precipitation for each month
     return precipitation
 
 
 def tick_analyze(data):
     """
     Isolate and process only necessary tick data
-    
+
     Args:
         data: Ticks dataframe, downloaded from neon_data_tools
     Returns:
         necessary_data: a pandas series with the total amount of tick
         for a month and the month as the row labels.
     """
-    #Convert contents of "collectDate" to a datetime object
+    # Convert contents of "collectDate" to a datetime object
     data["collectDate"] = pd.to_datetime(data["collectDate"])
-    data.set_index("collectDate", inplace = True)
+    data.set_index("collectDate", inplace=True)
 
-    #Sum the ticks for the month
+    # Sum the ticks for the month
     sum_ticks = data['individualCount'].resample('M').sum()
 
     return sum_ticks
