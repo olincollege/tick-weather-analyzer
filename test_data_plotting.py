996daf92121ff09357a198e30ccaeffa8ce820d6
Lili Baker <lilijbaker@gmail.com>
2022-03-28 12:30:11 -0400

    Add first linear regression test case

--- test_data_plotting.py
+++ test_data_plotting.py
@@ -2,17 +2,22 @@
 Test library functions to plot data
 """
 import pytest
+import numpy as np
 
 from data_plotting import (
     perform_linear_regression,
     get_correlation_coefficients,
-    data_to_array
+    data_to_array,
 )
 
 
 # Define sets of test cases.
 perform_linear_regression_cases = [
-    
+    # test simple linear relationship
+    ([np.array([1, 2, 3]), np.array([2, 4, 6])], (2, 0, 1)),
+
+    # test tick and humidity relationship
+    ([np.array([])])
 ]
 
 get_correlation_coefficients_cases = [
@@ -26,14 +31,14 @@ data_to_array_cases = [
 
 # Define standard testing functions to check functions' outputs given certain
 # inputs defined above.
-@pytest.mark.parametrize("data,values", perform_linear_regression)
+@pytest.mark.parametrize("data,values", perform_linear_regression_cases)
 def test_perform_linear_regression(data, values):
     """
     Test that perform_linear_regression returns expected values.
     Args:
         
     """
-    assert perform_linear_regression(data) == values
+    assert perform_linear_regression(data[0], data[1]) == values
 
 
 @pytest.mark.parametrize("data,coefficients", get_correlation_coefficients_cases)
