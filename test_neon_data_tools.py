3dda89cd24a0fd6f48a0577814933e148cd2fa6c
Cherry Pham <meokkmun@gmail.com>
2022-03-30 18:16:02 -0400

    add get_dates_cases and ran 4 tests

--- test_neon_data_tools.py
+++ test_neon_data_tools.py
@@ -6,56 +6,46 @@ import numpy as np
 
 from neon_data_tools import (
     create_stacked_dataframe,
+    get_dates,
 )
 
+# # Define sets of test cases.
+# create_stacked_dataframe_cases = [
+#     # test tick data
+#     ("D")
+# ]
 
-# Define sets of test cases.
-create_stacked_dataframe_cases = [
-    # test tick data
-    ("D")
+get_dates_cases = [
+    # Same start/end month and start/end year
+    ([1, 1, 2000, 2000], [['2000-01']]),
+    # Range of months in same year
+    ([5, 8, 1988, 1988], [['1988-05', '1988-06', '1988-07', '1988-08']]),
+    # Same month, different years
+    ([2, 2, 2000, 2003], [['2000-02'], ['2001-02'], ['2002-02'], ['2003-02']]),
+    # Same range of months, different years
+    ([4, 8, 2017, 2019], [['2017-04', '2017-05', '2017-06', '2017-07', '2017-08'],
+        ['2018-04', '2018-05', '2018-06', '2018-07', '2018-08'],
+        ['2019-04', '2019-05', '2019-06', '2019-07', '2019-08']]),
 ]
-
-get_correlation_coefficients_cases = [
-    # test simple case
-    ([["x_1", "x_2"], [np.array([1, 2, 3]), np.array([-1, -2, -3])],
-     np.array([2, 4, 6])], {"x_1": 1, "x_2": -1}),
-
-    # test correlations for all datasets
-    ([['Humidity', 'Precipitation'],
-    [np.array([66.18246313, 71.42232345, 68.20874646, 73.4316989,  71.90363023]),
-    np.array([1.45133333, 4.53193548, 3.569, 1.76935484, 2.14483871])],
-    np.array([3, 45, 38, 53, 99])], {"Humidity": .736, "Precipitation": 0.05}),
-]
-
 
 # Define standard testing functions to check functions' outputs given certain
 # inputs defined above.
-@pytest.mark.parametrize("data,values", create_stacked_dataframe_cases)
-def test_create_stacked_dataframe(data, values):
-    """
-    Test that perform_linear_regression returns expected values.
-    Args:
-        data: A list with the values being a list of the numpy arrays
-            of the independent data and a numpy array of the dependent data.
-        coefficients: A dictionary with the keys as the strings of the
-            names of the independent datasets, and the values as the
-            Pearson correlaction coefficients (r_value).
-    """
-    assert create_stacked_dataframe(data[0], data[1]) == values
+# @pytest.mark.parametrize("data,values", create_stacked_dataframe_cases)
+# def test_create_stacked_dataframe(data, values):
+#     """
+#     Test that perform_linear_regression returns expected values.
+#     Args:
+#         data: A list with the values being a list of the numpy arrays
+#             of the independent data and a numpy array of the dependent data.
+#         coefficients: A dictionary with the keys as the strings of the
+#             names of the independent datasets, and the values as the
+#             Pearson correlaction coefficients (r_value).
+#     """
+#     assert create_stacked_dataframe(data[0], data[1]) == values
 
-
-@pytest.mark.parametrize("data,coefficients", get_correlation_coefficients_cases)
-def test_get_correlation_coefficients(data, coefficients):
+@pytest.mark.parametrize("data,values", get_dates_cases)
+def test_get_dates(data, values):
     """
-    Test that get_correlation_coefficients returns correct coefficients.
-    Args:
-        data: A list with the values being a list of the string names
-            of the independent data, a list of the numpy arrays of the
-            independent data, and a numpy array of the dependent data.
-        coefficients: A dictionary with the keys as the strings of the
-            names of the independent datasets, and the values as the
-            Pearson correlaction coefficients (r_value).
-
     """
-    assert get_correlation_coefficients(
-        data[0], data[1], data[2]) == coefficients
+    assert get_dates(data[0], data[1], data[2], data[3]) == values
+
