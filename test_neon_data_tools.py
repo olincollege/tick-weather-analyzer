742d898a97fa6a375ea89b5526e739153edffaa0
Cherry Pham <meokkmun@gmail.com>
2022-03-30 21:51:06 -0400

    All test passes, pylint 10/10

--- test_neon_data_tools.py
+++ test_neon_data_tools.py
@@ -2,10 +2,8 @@
 Test neon_data_tools functions.
 """
 import pytest
-import numpy as np
 
 from neon_data_tools import (
-    create_stacked_dataframe,
     get_dates,
 )
 
@@ -18,16 +16,17 @@ GET_DATES_CASES = [
     ([2, 2, 2000, 2003], [['2000-02'], ['2001-02'], ['2002-02'], ['2003-02']]),
     # Same range of months, different years
     ([4, 8, 2017, 2019], [['2017-04', '2017-05', '2017-06', '2017-07', '2017-08'],
-        ['2018-04', '2018-05', '2018-06', '2018-07', '2018-08'],
-        ['2019-04', '2019-05', '2019-06', '2019-07', '2019-08']]),
+                          ['2018-04', '2018-05', '2018-06', '2018-07', '2018-08'],
+                          ['2019-04', '2019-05', '2019-06', '2019-07', '2019-08']]),
 ]
+
 
 @pytest.mark.parametrize("dates,dates_list", GET_DATES_CASES)
 def test_get_dates(dates, dates_list):
     """
     Check that the return value provides the range of desired dates
 
-    Args: 
+    Args:
         dates: A list of desired values, corresponding to: starting month,
             ending month, starting year, ending year for the dates list
         dates_list: A list containing smaller lists of a range of desired months in
