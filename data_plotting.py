9dfefbd478efb6b97943a02a8352b39f1fa3fe16
Lili Baker <lilijbaker@gmail.com>
2022-03-30 22:03:37 -0400

    Edit data_plotting to conform to style guidelines

--- data_plotting.py
+++ data_plotting.py
@@ -1,11 +1,9 @@
 """
 Data visulization tools
 """
-import pandas as pd
-from neon_data_tools import download_data
 import matplotlib.pyplot as plt
 from scipy import stats
-import numpy as np
+
 
 def perform_linear_regression(independent_data, tick_data):
     """
@@ -14,16 +12,22 @@ def perform_linear_regression(independent_data, tick_data):
     Args:
         independent_data: A numpy array of the independent data.
         tick_data: A numpy array of the total number of ticks per month.
-    
+
     Returns:
-        Floats representing the slope, intercept, and Pearson correlation coefficient 
-        of the linear relationship between the data.
+        Floats representing the slope, intercept, and Pearson correlation
+        coefficient of the linear relationship between the data.
     """
-    slope, intercept, r_value, p_value, standard_error = stats.linregress(independent_data, tick_data)
+    # perform linear regression
+    slope, intercept, r_value = stats.linregress(
+        independent_data, tick_data)[:3]
+    # round the values to four decimal places for readability
     slope = round(slope, 4)
     intercept = round(intercept, 4)
     r_value = round(r_value, 4)
+
+    # return only the needed values
     return slope, intercept, r_value
+
 
 def plot_line_of_best_fit(independent_data, tick_data):
     """
@@ -36,13 +40,23 @@ def plot_line_of_best_fit(independent_data, tick_data):
     Args:
         independent_data: A numpy array of the independent data.
         tick_data: A numpy array of the total number of ticks per month.
-    
+
     """
-    slope, intercept, r_value = perform_linear_regression(independent_data, tick_data)
+    # perform linear regression
+    slope, intercept = perform_linear_regression(
+        independent_data, tick_data)[:2]
+
+    # plot the datapoints using a scatter plot
     plt.scatter(independent_data, tick_data)
+
+    # plot the line of best fit using the values from linear regression
     plt.plot(independent_data, slope * independent_data + intercept)
-    plt.text(min(independent_data) + 1, max(tick_data) - 0.3, 'y = ' + '{:.2f}'.format(intercept) + ' + {:.2f}'.format(slope) + 'x', size=12)
+
+    # add the equation to the plot
+    plt.text(min(independent_data) + 1, max(tick_data) - 0.3, 'y = ' +
+             '{:.2f}'.format(intercept) + ' + {:.2f}'.format(slope) + 'x', size=12)
     plt.show()
+
 
 def plot_correlation_coefficients(datasets, independent_datasets, tick_data):
     """
@@ -51,7 +65,7 @@ def plot_correlation_coefficients(datasets, independent_datasets, tick_data):
     Plots a bar graph with each bar representing the correlation
     coefficient of the given dataset and the tick_data. Uses
     get_correlation_coefficents to determine the correlation coefficients.
-    
+
     Args:
         datasets: A list of strings that represent the names of the
             independent data in their respective order.
@@ -59,12 +73,18 @@ def plot_correlation_coefficients(datasets, independent_datasets, tick_data):
         tick_data: A numpy array of the total number of ticks per month.
     """
     bar_width = 0.35
-    correlations = get_correlation_coefficients(datasets, independent_datasets, tick_data)
+
+    # get the correlation coefficents for the datasets
+    correlations = get_correlation_coefficients(
+        datasets, independent_datasets, tick_data)
+
+    # plot the bar graphs of the correlation coefficients
     plt.bar(datasets, list(correlations.values()), bar_width)
     plt.xlabel("Dataset")
     plt.ylabel("Correlation Coefficient")
     plt.title("Correlation Coefficients of Datasets and Tick Population")
     plt.show()
+
 
 def get_correlation_coefficients(datasets, independent_datasets, dependendent_dataset):
     """
@@ -74,44 +94,49 @@ def get_correlation_coefficients(datasets, independent_datasets, dependendent_da
     representing all of the datasets and their given correlation
     coefficient. Uses perform_linear_regression to get the
     r_value, which is the Pearson correlation coefficent.
-    
+
     Args:
         datasets: A list of strings that represent the names of the
             independent data in their respective order.
         independent_datasets: A list of numpy arrays of the independent data.
         tick_data: A numpy array of the total number of ticks per month.
-    
+
     Returns:
         correlations: A dictionary with each key being the string representing
         the name of the dataset, and the key being the respective correlation
         coefficient.
     """
     correlations = {}
+    # get the correlation coefficient for each dataset
     for index, data in enumerate(independent_datasets):
-        slope, intercept, r_value  = perform_linear_regression(data, dependendent_dataset)
+        r_value = perform_linear_regression(
+            data, dependendent_dataset)[2]
+
+        # set the value at the key of the dataset to the correlation coefficient
         correlations[datasets[index]] = r_value
     return correlations
 
-def plot_line_chart(time, dependent_data, value):
+
+def plot_combined_line_graph(time, dependent_datasets, values):
     """
     Plot the data over time.
 
-    Plots a line graph of the dependent_data over time.
-    
+    Plots a line graph of the data over time.
+
     Args:
         time: A numpy array of datatime objects.
         dependent_data: A numpy array of the values at
             each time step.
+        values: A list of strings containing the names of
+            the datasets.
     """
-    plt.plot(time, dependent_data)
-    plt.title(f'{value} data over time')
-    plt.xlabel('Date')
-    plt.ylabel(f'{value}')
-
-def plot_combined_line_graph(time, dependent_datasets, values):
+    # set the colors of the lines to be different
     colors = ['r', 'c', 'g']
+
+    # for each dataset, plot the data over time
     for index, value in enumerate(values):
-        plt.plot(time, dependent_datasets[index], colors[index], label=values[index])
+        plt.plot(time, dependent_datasets[index],
+                 colors[index], label=value)
     plt.title('Ticks , Relative Humidity, and Precipitation over time')
     plt.xlabel('Date')
     plt.ylabel("Value")
